0e75f46248255bf3a3856ec3621d256fca2bd8ba
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2023-08-07 16:16:15 +0200

    Update password-generator.c

--- src/password-generator.c
+++ src/password-generator.c
@@ -6,73 +6,65 @@
 
 #include "randomUtils.h"
 
-#define ANSI_RED        "\x1b[31m"
-#define ANSI_GREEN      "\x1b[32m"
-#define ANSI_YELLOW     "\x1b[33m"
-#define ANSI_BLUE       "\x1b[34m"
-#define ANSI_MAGENTA    "\x1b[35m"
-#define ANSI_CYAN       "\x1b[36m"
-#define ANSI_RESET      "\x1b[0m"
-
-char *mergestring(char *lowercase, char *uppercase, char *symbols) {
-
-    int charCount = strlen(lowercase) + strlen(uppercase) + strlen(symbols);
-
-    char *str = malloc(sizeof(char) * charCount);
-
-    strcat(str, uppercase);
-    strcat(str, lowercase);
-    strcat(str, symbols);
-
-    return str;
+char* mergestring(lowercase, uppercase, symbols)
+char* lowercase;
+char* uppercase;
+char* symbols;
+{
+	int charCount = strlen(lowercase) + strlen(uppercase) + strlen(symbols);
+	char *str = malloc(sizeof(char) * charCount);
+	strcat(str, uppercase);
+	strcat(str, lowercase);
+	strcat(str, symbols);
+	return str;
 }
 
-char *scrambler(char *input, int length) {
-
-    char *output = input;
-
-    char oldChar;
-    char newChar;
-    int index = 0;
-
-    for (int i = 0; i <= (length-1); i++) {
-
-        index = getRandomInt(0, length);
-
-        oldChar = output[i];
-        newChar = output[index];
-
-        output[i]     = newChar;
-        output[index] = oldChar;
-    }
-    return output;
+char* scrambler(input, length)
+char* input;
+int length;
+{
+	char* output = input;
+	char oldChar;
+	char newChar;
+	int index = 0;
+	for (int i = 0; i <= (length-1); i++) {
+		index = getRandomInt(0, length);
+		oldChar = output[i];
+		newChar = output[index];
+		output[i] = newChar;
+		output[index] = oldChar;
+	}
+	return output;
 }
 
-void passwordGenerator(int length) {
-
-    srand(time(0));
-
-    int *amount = intSplitter(3, length);
-    int lowerCount = amount[0];
-    int upperCount = amount[1];
-    int symCount   = amount[2];
-
-    char *lowerChars = malloc(sizeof(int) * lowerCount);
-    char *upperChars = malloc(sizeof(int) * upperCount);
-    char *symChars   = malloc(sizeof(int) * symCount);
-
-    strcpy(lowerChars, getRandomChars(lowerCount, false));
-    strcpy(upperChars, getRandomChars(upperCount, true));
-    strcpy(symChars,   getRandomSymbols(symCount));
-    
-    char *password = mergestring(lowerChars, upperChars, symChars);
-    char *scrambleChars = scrambler(password, length);
-
-    free(lowerChars);
-    free(upperChars);
-    free(symChars);
-
-    printf(ANSI_YELLOW "Password:      " ANSI_BLUE "%s\n" ANSI_RESET, scrambleChars);
-
-    return;
+void passwordGenerator(length)
+int length;
+{
+	int* amount;
+	int lowerCount;
+	int upperCount;
+	int symCount;
+	char* lowerChars;
+	char* upperChars;
+	char* symChars;
+	char* password;
+	char* scrambleChars;
+	srand(time(0));
+	amount = intSplitter(3, length);
+	lowerCount = amount[0];
+	upperCount = amount[1];
+	symCount   = amount[2];
+	lowerChars = malloc(sizeof(int) * lowerCount);
+	upperChars = malloc(sizeof(int) * upperCount);
+	symChars   = malloc(sizeof(int) * symCount);
+	strcpy(lowerChars, getRandomChars(lowerCount, false));
+	strcpy(upperChars, getRandomChars(upperCount, true));
+	strcpy(symChars,   getRandomSymbols(symCount));
+	password = mergestring(lowerChars, upperChars, symChars);
+	scrambleChars = scrambler(password, length);
+	free(lowerChars);
+	free(upperChars);
+	free(symChars);
+	printf("Password:    %s\n", scrambleChars);
+	return;
 }
