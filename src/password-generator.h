3e78b7b0e3a06e3025359f2b237920c729f587ff
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2023-08-07 16:16:40 +0200

    Update password-generator.h

--- src/password-generator.h
+++ src/password-generator.h
@@ -1,13 +1,7 @@
 #ifndef PASSWORGENERRATOR_H
 #define PASSWORGENERRATOR_H
-
-// Standard Libs
 #include <stdbool.h>
-
-char *mergestring(char *lowercase, char *uppercase, char *symbols);
-
-char *scrambler(char *input, int length);
-
+char* mergestring(char *lowercase, char *uppercase, char *symbols);
+char* scrambler(char *input, int length);
 void passwordGenerator(int length);
-
 #endif
