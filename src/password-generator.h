171efc384c1e50839d326db0b3887852e2238817
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-02-06 00:06:42 +0100

    Delete src/password-generator.h

--- src/password-generator.h
+++
@@ -1,7 +0,0 @@
-#ifndef PASSWORGENERRATOR_H
-#define PASSWORGENERRATOR_H
-#include <stdbool.h>
-char* mergestring(char *lowercase, char *uppercase, char *symbols);
-char* scrambler(char *input, int length);
-void passwordGenerator(int length);
-#endif
