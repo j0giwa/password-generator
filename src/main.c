7a73602bd6439f12465a3c81cabd88c3212adbe5
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-02-06 00:07:37 +0100

    Cleanup of past mistakes
    The project was far more complex than necessary.
    I streamlined the codebase to a sane scope, in order to avoid making things more complicated than they need to be.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <ctype.h>
 #include <string.h>
+#include <time.h>
 
-#include "password-generator.h"
+/**
+ * Produces a random 16-bit integer
+ */
+int get_next_int(min, max) 
+        int min;
+        int max;
+{
+	int num = (rand() % (min - max + 1)) + min;
+	return num;
+}
+
+/**
+ * Prduced a char* of random ascii characters
+ */
+char* get_ascii(length)
+        int length;
+{
+    	int ascii_val, first_char, last_char;
+    	char *ascii_char, *ascii_string;
+
+	if (length <= 0)
+		return "";
+
+    	ascii_char = (char*) malloc(sizeof(int) * 1);
+    	ascii_string = (char*) malloc(sizeof(int) * length);
+    	first_char = 32;
+    	last_char = 127;
+
+        for (int i = 0; i < length; i++) {
+		ascii_val = get_next_int(first_char, last_char);
+		snprintf(ascii_char, 2, "%c", ascii_val);
+		strncpy(&ascii_string[i], ascii_char, 2);
+	}
+
+	return ascii_string;
+}
+
+/**
+ * Prints a semi-secure password to stdout
+ */
+void passwd_gen(length)
+        int length;
+{
+	char *passwd;
+	srand(time(0));
+	passwd = get_ascii(length);
+	printf("Password: %s\n", passwd);
+	return;
+}
 
 int main(argc, argv)
-int argc;
-char** argv;
+        int argc;
+        char** argv;
 {
-	int length;
 	if (argc == 0)
 		return 2;
 	if (argc > 3)
 		return 2;
 	if (argc == 1) {
-		length = 8;
-		passwordGenerator(length);
+		passwd_gen(8);
 		return EXIT_SUCCESS;
 	}
+
+	int length;
+
 	for (int arg = 1; arg < argc; arg++) {
 		if (strcmp("-v", argv[arg]) == 0) {
 			printf("Password-generator\nVersion: 1.0.0.0");
@@ -43,6 +92,8 @@ char** argv;
 			continue;
 		}
 	}
-	passwordGenerator(length);
+
+	passwd_gen(length);
 	return EXIT_SUCCESS;
 }
+
